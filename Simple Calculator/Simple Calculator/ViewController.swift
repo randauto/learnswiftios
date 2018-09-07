a226145745d6add5d05b0783b9898ffb810fa5c4
tuanlq <tuanlqhvu@gmail.com>
2018-09-07 22:46:39 +0700

    add simple cal

---
+++ Simple Calculator/Simple Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Calculator
+//
+//  Created by Tuan Le Quoc  on 9/7/18.
+//  Copyright © 2018 NGUYEN THE ANH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
